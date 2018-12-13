72e9c5c264d19a8ce112f0ab4fe22835224ddcb6
jare4857 <jacob.reed1998@gmail.com>
2018-12-12 17:10:11 -0700

    attemtping to set up app similar to lab 9

---
+++ product/database.js
@@ -0,0 +1,14 @@
+var pgp = require('pg-promise')();
+
+const dbConfig = {
+   host: 'localhost',
+   port: 5432,
+   database: 'project_3308',
+   user: 'postgres',
+   password: '1234' // TODO: Fill in your PostgreSQL password here.
+                // Use empty string if you did not set a password
+};
+
+var db = pgp(dbConfig);
+
+module.exports = db;
---
+++ product/server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+
+
+var expressValidator = require('express-validator');
+app.use(expressValidator());
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var methodOverride = require('method-override');
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        var method = req.body._method;
+        delete req.body._method;
+        return method
+    }
+}));
+
+var flash = require('express-flash');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+app.use(cookieParser('csci3308'));
+app.use(session({
+    secret: 'csci3308',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {maxAge: 60000}
+}));
+app.use(flash());
+
+var index = require('./routes/index');
+var store = require('./routes/store');
+app.use('/', index);
+app.use('/store', store);
+
+var port = 4000;
+app.listen(port, function () {
+    console.log('Server running on http://localhost:' + port)
+});
